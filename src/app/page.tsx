9f58d698635da7042447fb8f1b942e99751fefd9
Douglas Jasper <doug.jasper@trios.com>
2026-04-29 09:01:04 -0400

    Using Route Handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { ContactForm } from '@/components/ContactForm';
+
 export default function Home() {
   return (
     <main>
+      <h2>Contact Us</h2>
+      <p>
+        If you enter your details we will get back to you a soon as we can.
+      </p>
+      <ContactForm />
     </main>
   );
 }
