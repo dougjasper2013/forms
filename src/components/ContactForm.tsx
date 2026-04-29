974cbed95a714f2080a04fd77012d3940bf37db4
Douglas Jasper <doug.jasper@trios.com>
2026-04-29 10:20:00 -0400

    UseActionState Version

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,37 +1,42 @@
 'use client';
-// import { useRouter } from 'next/navigation';
+import { useActionState } from 'react';
 import { insertContact } from '@/data/insertContact';
-import { Contact } from '@/data/schema';
 
 export function ContactForm() {
-  // const { push } = useRouter();
-//   async function handleAction(
-//     formData: FormData,
-//   ) {
-//     const contact = Object.fromEntries(
-//       formData,
-//     ) as Contact;
-//     const response = await fetch('api', {
-//       method: 'POST',
-//       body: JSON.stringify(contact),
-//     });
-//     if (!response.ok) {
-//       console.error('Something went wrong');
-//       return;
-//     }
-//     push(
-//       '/thanks/?name=' +
-//         encodeURIComponent(contact.name),
-//     );
-//   }
+  const [
+    { ok, error, errors, formData },
+    formAction,
+    isPending,
+  ] = useActionState(insertContact, {
+    ok: false,
+    error: '',
+    errors: {
+      name: null,
+      email: null,
+      reason: null,
+    },
+    formData: new FormData(),
+  });
+
   return (
-    <form action={insertContact}>
+    <form action={formAction}>
       <div className="field">
         <label htmlFor="name">Your name</label>
         <input
           type="text"
           id="name"
           name="name"
+          defaultValue={
+            (formData.get('name') ?? '') as string
+          }
+          aria-invalid={
+            errors.name ? 'true' : 'false'
+          }
+          aria-describedby="name-error"
+        />
+        <FieldError
+          serverError={errors.name}
+          errorId="name-error"
         />
       </div>
       <div className="field">
@@ -42,13 +47,36 @@ export function ContactForm() {
           type="email"
           id="email"
           name="email"
+          defaultValue={
+            (formData.get('email') ??
+              '') as string
+          }
+          aria-invalid={
+            errors.email ? 'true' : 'false'
+          }
+          aria-describedby="email-error"
+        />
+        <FieldError
+          serverError={errors.email}
+          errorId="email-error"
         />
       </div>
       <div className="field">
         <label htmlFor="reason">
           Reason you need to contact us
         </label>
-        <select id="reason" name="reason">
+        <select
+          id="reason"
+          name="reason"
+          defaultValue={
+            (formData.get('reason') ??
+              '') as string
+          }
+          aria-invalid={
+            errors.reason ? 'true' : 'false'
+          }
+          aria-describedby="reason-error"
+        >
           <option value=""></option>
           <option value="Support">Support</option>
           <option value="Feedback">
@@ -56,14 +84,52 @@ export function ContactForm() {
           </option>
           <option value="Other">Other</option>
         </select>
+        <FieldError
+          serverError={errors.reason}
+          errorId="reason-error"
+        />
       </div>
       <div className="field">
         <label htmlFor="notes">
           Additional notes
         </label>
-        <textarea id="notes" name="notes" />
+        <textarea
+          id="notes"
+          name="notes"
+          defaultValue={
+            (formData.get('notes') ??
+              '') as string
+          }
+        />
       </div>
-      <button type="submit">Submit</button>
+      {!ok && (
+        <p role="alert" className="error">
+          {error}
+        </p>
+      )}
+      {isPending && (
+        <p role="alert">Saving ...</p>
+      )}
+      <button type="submit" disabled={isPending}>
+        Submit
+      </button>
     </form>
+  );
+}
+type Err = { message?: string } | null;
+function FieldError({
+  serverError,
+  errorId,
+}: {
+  serverError: Err;
+  errorId: string;
+}) {
+  if (!serverError) {
+    return null;
+  }
+  return (
+    <div id={errorId} role="alert">
+      {serverError.message}
+    </div>
   );
 }
--- src/data/insertContact.ts
+++ src/data/insertContact.ts
@@ -6,20 +6,42 @@ import {
 } from '@libsql/client';
 import { redirect } from 'next/navigation';
 import { contactSchema } from './schema';
+import { z } from 'zod';
+
+type Err = { message: string };
+type FieldErrors = {
+  name: Err | null;
+  email: Err | null;
+  reason: Err | null;
+};
+type ActionState = {
+  ok: boolean;
+  error: string;
+  formData: FormData;
+  errors: FieldErrors;
+};
 
 export async function insertContact(
+  previousState: ActionState,
   formData: FormData,
 ) {
   const parsedResult = contactSchema.safeParse(
     Object.fromEntries(formData),
   );
   if (!parsedResult.success) {
-    return;
+    return {
+      ok: false,
+      error:
+        'Unable to save - invalid field values',
+      errors: formatZodErrors(parsedResult.error),
+      formData,
+    };
   }
   const { name, email, reason, notes } =
     parsedResult.data;
   let client: Client | undefined;
   let ok = true;
+  let error = '';
   try {
     client = createClient({
       url: process.env.DB_URL ?? '',
@@ -30,6 +52,7 @@ export async function insertContact(
     });
   } catch {
     ok = false;
+    error = 'Problem saving form';
   }
   if (client) {
     client.close();
@@ -39,4 +62,42 @@ export async function insertContact(
       `/thanks/?name=${encodeURIComponent(name)}`,
     );
   }
+  return {
+    ok,
+    error,
+    formData,
+    errors: {
+      name: null,
+      email: null,
+      reason: null,
+    },
+  };
+}
+
+function formatZodErrors(error: z.ZodError) {
+  const formattedErrors: FieldErrors = {
+    name: null,
+    email: null,
+    reason: null,
+  };
+  for (const [key, value] of Object.entries(
+    error.flatten().fieldErrors,
+  )) {
+    if (Array.isArray(value)) {
+      if (key === 'name') {
+        formattedErrors.name = {
+          message: value[0],
+        };
+      } else if (key === 'email') {
+        formattedErrors.email = {
+          message: value[0],
+        };
+      } else if (key === 'reason') {
+        formattedErrors.reason = {
+          message: value[0],
+        };
+      }
+    }
+  }
+  return formattedErrors;
 }
