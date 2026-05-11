ab056ab23a2c95895a7a5333a3963502c5e40f6a
Douglas Jasper <doug.jasper@trios.com>
2026-05-11 10:33:00 -0400

    Added and

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main>
       <h2>Contact Us</h2>
       <p>
-        If you enter your personal details we will get back to you a soon as we can.
+        If you enter your personal details and we will get back to you a soon as we can.
       </p>
       <ContactForm />
     </main>
